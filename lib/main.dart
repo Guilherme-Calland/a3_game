8ed3a47d0a4e69e152c552fb621151afa1fa37d9
guilhermow <gui.crb0@gmail.com>
2020-05-10 23:38:51 -0300

    andando p esquerda e direita feito um mocinho

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,15 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
   void initState() {
     super.initState();
     animationController = AnimationController(
-      duration: Duration(milliseconds: 1000),
-      vsync: this
+      duration: Duration(milliseconds: 2000),
+      vsync: this,
+      //between 0 and 1
+      value: 0.5
     );
 
     animation = Tween<double>(
-      begin: 0.0,
-      end: 5.0
+      begin: -200.0,
+      end: 200.0,
     ).animate(animationController)
     ..addListener(() {
       setState(() {
@@ -40,29 +42,55 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
+
+    print(animation.value);
+
     return Scaffold(
       backgroundColor: Color(0xff252525) ,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Center(
-            child: Transform.scale(
-              scale: 1 + animation.value,
+            child: Transform.translate(
+              offset: Offset(animation.value, 0),
               child: Image.asset(
                 'images/knight.png'
               )
             )
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GameButton(
+                title: 'go left',
+                color: Colors.green,
+                onPressed: (){
+                  animationController.reverse();
+                }
+              ),
+              GameButton(
+                title: 'stop',
+                color: Colors.red,
+                onPressed: (){
+                  animationController.stop();
+                }
+              ),
+              GameButton(
+                title: 'go right',
+                color: Colors.green,
+                onPressed: (){
+                  animationController.forward();
+                }
+              ),
+            ],
+          ),
           GameButton(
-            title: 'start animation',
-            color: Colors.green,
+            title: 'jump',
+            color: Colors.blue,
             onPressed: (){
-              animation.value == 0 ?
-              animationController.forward() :
-              animationController.reverse(from: animation.value/5);
-            },
-
-          )
+              animationController.forward();
+            }
+          ),
         ],
       )
     );
